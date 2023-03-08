42c5d3b1345aa45d912c5458587b250c37bd82ad
wubing-hit <wubing_hit@hotmail.com>
2023-03-08 05:53:35 +0000

    add greeting controler

--- src/main/java/com/epic/modem/ModemApplication.java
+++ src/main/java/com/epic/modem/ModemApplication.java
@@ -2,12 +2,21 @@ package com.epic.modem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@RestController
 public class ModemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModemApplication.class, args);
 	}
 
+	private static final String template = "Hello, %s!";
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue="World") String name) {
+		return String.format(template, name);
+	}
 }
