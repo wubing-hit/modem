69460c3f02aa1c971e72dd2f4e44fd393042f2c2
Bing <tarzan@epic.com>
2023-03-08 13:34:00 +0800

    initial version

---
+++ src/main/java/com/epic/modem/ModemApplication.java
@@ -0,0 +1,13 @@
+package com.epic.modem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epic/modem/ModemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epic.modem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
